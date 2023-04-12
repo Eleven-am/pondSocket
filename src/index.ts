0e5e0e3c4db08ec2e28f22774214db4f7300c952
royossai <royossai@yahoo.com>
2023-04-12 21:59:46 +0200

    exported channel

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import { PondChannel as Channel } from './server/pondChannel/pondChannel';
 import { PondSocket } from './server/server/pondSocket';
 
 export default PondSocket;
+export { Channel };
