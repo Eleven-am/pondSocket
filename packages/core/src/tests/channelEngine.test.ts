5771e387d4858411d4a4bab197f3e75adb6a9c1d
Roy OSSAI <royossai@yahoo.com>
2025-04-29 17:46:05 +0200

    refactor channelEngine and lobbyEngine to implement channel deletion and update tests

--- packages/core/src/tests/channelEngine.test.ts
+++ packages/core/src/tests/channelEngine.test.ts
@@ -519,11 +519,13 @@ describe('ChannelEngine', () => {
         it('should close the channel', () => {
             // Using a different approach since we can't spy on private methods
             // Mock the close method
-            const closeSpy = jest.spyOn(mockLobbyEngine as any, 'deleteChannel').mockImplementation(() => {});
+            const closeSpy = jest.spyOn(channelEngine as any, 'close').mockImplementation(() => {});
+            const deleteChannel = jest.spyOn(mockLobbyEngine as any, 'deleteChannel').mockImplementation(() => {});
 
             channelEngine.destroy();
 
             expect(closeSpy).toHaveBeenCalled();
+            expect(deleteChannel).toHaveBeenCalledWith(channelEngine.name);
         });
     });
 
